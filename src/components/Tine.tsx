c9ed276c5c67c949f0b13cabf0757fa9958e7ad8
slava <vvkayushkin@gmail.com>
2023-05-28 12:58:54 -0700

    changed how audio snippet time is decided

--- src/components/Tine.tsx
+++ src/components/Tine.tsx
@@ -1,49 +1,34 @@
-import { FC } from "react"
+import { FC, useRef } from "react"
 
 type TineProps = {
-  note: string
+  note: 'a' | 'b' | 'c' | 'd' | 'e' | 'f' | 'g'
 }
 
 const Tine: FC<TineProps> = (props): JSX.Element => {
-  // let tineSound = new Audio('537519__a_curious_coder__kalimba-scale-descending.wav')]
+  // let tineSound = useRef(document.createElement('HTMLAudioElement'))
+  // ...
+  // let tineSound = new Audio('537519__a_curious_coder__kalimba-scale-descending.wav')
 
   const soundPath = ''
 
   const play = () => {
-    let audio: HTMLAudioElement = document.getElementById(`note ${props.note}`) as HTMLAudioElement
+    let audio: HTMLAudioElement = document.getElementById(`note${props.note}`) as HTMLAudioElement
     audio.play()
   }
-
+  
   const noteTimes = () => {
-    let times = [0, 0]
-    console.log(props.note)
-    switch (props.note) {
-      case 'a':
-        times = [0, 0.4]
-        break;
-      case 'b':
-        times = [0.4, 0.8]
-        break;
-      case 'c':
-        times = [0.8, 1.2]
-        break;
-      case 'd':
-        times = [1.2, 1.6]
-        break;
-      case 'e':
-        times = [1.6, 2.0]
-        break;
-      case 'f':
-        times = [2.0, 2.4]
-        break;
-      case 'g':
-        times = [2.4, 2.8]
-        break;
-      default:
-        return null
-    }
+    let time = {
+    'a': [0, 0.3],
+    'b': [0.3, 0.6],
+    'c': [0.3, 0.6],
+    'd': [0.3, 0.6],
+    'e': [0.3, 0.6],
+    'f': [0.3, 0.6],
+    'g': [0.3, 0.6]
+    }[props.note]
+
     //string to add to audio src to limit time
-    let timesString = `#t=${times[0]},${times[1]}`
+    let timesString = `#t=${time[0]},${time[1]}`
     return timesString
   }
 
@@ -51,10 +36,12 @@ const Tine: FC<TineProps> = (props): JSX.Element => {
     {console.log(noteTimes())}
     <input type="button" value={`PLAY ${props.note}`} onClick={play} />
     <audio
-      id={`note ${props.note}`}
+      id={`note${props.note}`}
       src={`537519__a_curious_coder__kalimba-scale-descending.wav${noteTimes()}`}>
     </audio>
     {/* <button onClick={tineSound.play}>TineSound</button> */}
+
+    {/* <button onClick={tineSound.current.play}>TineSound</button> */}
   </>
 }
 
